5a6f88b8613e59c51f73131901c4937bf5a362b6
regiplgmikp <regiplasgo@gmail.com>
2025-05-07 06:43:44 -0600

    se agregan expresiones regulares de tockens

--- archivo_lex.py
+++ archivo_lex.py
@@ -22,41 +22,41 @@ tokens = (
 
 # Definir etiquetas para los tokens
 
-t_PRODUCTOS_S = r''
-t_PRODUCTOS_E = r''
+t_PRODUCTOS_S = r'<PRODUCTOS>'
+t_PRODUCTOS_E = r'</PRODUCTOS>'
 
-t_PRODUCTO_S = r''
-t_PRODUCTO_E = r''
+t_PRODUCTO_S = r'<PRODUCTO>'
+t_PRODUCTO_E = r'</PRODUCTO>'
 
-t_ID_S = r''
-t_ID_E = r''
+t_ID_S = r'<ID>'
+t_ID_E = r'</ID>'
 
-t_NOMBRE_S = r''
-t_NOMBRE_E = r''
+t_NOMBRE_S = r'<NOMBRE>'
+t_NOMBRE_E = r'</NOMBRE>'
 
-t_MARCA_S = r''
-t_MARCA_E = r''
+t_MARCA_S = r'<MARCA>'
+t_MARCA_E = r'</MARCA>'
 
-t_CATEGORIA_S = r''
-t_CATEGORIA_E = r''
+t_CATEGORIA_S = r'<CATEGORIA>'
+t_CATEGORIA_E = r'</CATEGORIA>'
 
-t_PRECIO_S = r''
-t_PRECIO_E = r''
+t_PRECIO_S = r'<PRECIO>'
+t_PRECIO_E = r'</PRECIO>'
 
-t_STOCK_S = r''
-t_STOCK_E = r''
+t_STOCK_S = r'<STOCK>'
+t_STOCK_E = r'</STOCK>'
 
-t_DESCRIPCION_S = r''
-t_DESCRIPCION_E = r''
+t_DESCRIPCION_S = r'<DESCRIPCION>'
+t_DESCRIPCION_E = r'</DESCRIPCION>'
 
-t_INGREDIENTES_S = r''
-t_INGREDIENTES_E = r''
+t_INGREDIENTES_S = r'<INGREDIENTES>'
+t_INGREDIENTES_E = r'</INGREDIENTES>'
 
-t_INGREDIENTE_S = r''
-t_INGREDIENTE_E = r''
+t_INGREDIENTE_S = r'<INGREDIENTE>'
+t_INGREDIENTE_E = r'</INGREDIENTE>'
 
 # Texto entre etiquetas
-t_TEXT =r''
+t_TEXT =r'[A-ÿ0-9;,\._ ]+'
 
 # Definir una regla para ignorar espacios en blanco y saltos de línea
 t_ignore = ' \t\n'
